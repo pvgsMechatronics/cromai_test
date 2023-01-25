0d72d61986c6742b4d2f77f7537d5a7d99ef2c56
Paulo Vitor Gomes da Silva <paulo.vitor.pessoal@proton.me>
2023-01-24 21:46:59 -0300

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,9 +2,9 @@
 from os import getpid #função necessária para retornar a id do processo do script
 from time import sleep #função necessária para o delay
 
-arquivo = open("pid.txt", "w")
+arquivo = open("pid.txt", "w") 
 arquivo.write(str(getpid())) #busca o id do processo, converte em string e a escreve no arquivo aberto
-arquivo.close 
+arquivo.close() 
 
 for i in range(3):
     print("2: I am alive") #exibe mensagem de que o processo está sendo executado
